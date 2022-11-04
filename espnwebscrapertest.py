2adba630fb28454eb0d08c257703be6a8bf04a67
Anubhav Kundu <tuj00212@temple.edu>
2022-11-04 01:10:25 -0400

    Changed to nba

--- espnwebscrapertest.py
+++ espnwebscrapertest.py
@@ -9,10 +9,10 @@ options = Options()
 options.headless = True
 
 
-nfl_schedule = 'https://www.espn.com/nfl/schedule'
+nba_schedule = 'https://www.espn.com/nba/schedule'
 driver = webdriver.Chrome(options = options)
-driver.get(nfl_schedule)
+driver.get(nba_schedule)
 
-nfl_date = driver.find_element(By.CLASS_NAME, 'Table__Title') #Prints current date from NFL schedule
-print(nfl_date.text)
+nba_date = driver.find_element(By.CLASS_NAME, 'Table__Title') #Prints current date from NFL schedule
+print(nba_date.text)
 driver.close()
