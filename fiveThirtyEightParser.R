b22a457743c58aa62eb365390c3fd59baa837861
jaredStef <jaredStef@iCloud.com>
2022-11-30 13:01:10 -0500

    Commit before pres

--- fiveThirtyEightParser.R
+++ fiveThirtyEightParser.R
@@ -2,15 +2,16 @@ library(tidyverse)
 
 fiveThirtyEight_execute <- function(){
     #538 Data:
-    FiveThirtyEight_Predictions <- read_csv("https://projects.fivethirtyeight.com/nba-model/nba_elo.csv")
+    FiveThirtyEight_Predictions <- read.csv("https://projects.fivethirtyeight.com/nba-model/nba_elo.csv")
 
     #Cleaning:
     FiveThirtyEight_Predictions <- FiveThirtyEight_Predictions %>%
-      dplyr::select(date, season, team1, team2, elo_prob1) %>%
-      dplyr::rename(fivethirtyeight_home_wp = elo_prob1)
+      dplyr::select(date, season, team1, team2, raptor_prob1) %>%
+      dplyr::rename(fivethirtyeight_home_wp = raptor_prob1)
 
     head(FiveThirtyEight_Predictions)
     write.csv(FiveThirtyEight_Predictions, 'FiveThirtyEight.csv')
 }
+
 
 
